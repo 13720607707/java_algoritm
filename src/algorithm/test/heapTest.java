b4cd3eebd9fd177eb1f4f90d8c2f24d40a7708a2
zhengqijinyu <1454501387@qq.com>
2021-03-23 21:44:03 +0800

    堆

---
+++ src/algorithm/test/heapTest.java
@@ -0,0 +1,24 @@
+package algorithm.test;
+
+import algorithm.heap.heap;
+
+public class heapTest {
+    public static void main(String[] args) {
+        //创建堆对象
+        heap<String> heap = new heap<>(10);
+        //往堆中存入字符串数据
+        heap.insert("A");
+        heap.insert("B");
+        heap.insert("C");
+        heap.insert("D");
+        heap.insert("E");
+        heap.insert("F");
+        heap.insert("G");
+
+        //通过循环从堆中删除数据
+        String result = null;
+        while((result = heap.delMax())!=null){
+            System.out.print(result+" ");
+        }
+    }
+}
